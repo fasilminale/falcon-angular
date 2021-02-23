749afcae171eed67cfafd84ee314db04481f710b
Kooper Smith <kooper.smith@cardinalhealth.com>
2021-02-23 14:43:12 -0500

    [FAL-55] added health-check

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HealthCheckComponent } from './health-check/health-check.component';
+import {WebServices} from './services/web-services';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HealthCheckComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [WebServices],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
