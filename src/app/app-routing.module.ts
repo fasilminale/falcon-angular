1f2b03b0827004286e5dd0a3fb222ec292115a32
Kooper Smith <kooper.smith@cardinalhealth.com>
2021-02-25 12:14:26 -0500

    [FAL-77] wip

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {InvoiceListPageComponent} from './pages/invoice-list-page/invoice-list-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/invoices', pathMatch: 'full'},
+  {path: 'invoices', component: InvoiceListPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,15 @@ import { HealthCheckComponent } from './health-check/health-check.component';
 import {WebServices} from './services/web-services';
 import {HttpClientModule} from '@angular/common/http';
 import {ButtonModule, NavigationModule, ProgressModule} from '@elm/elm-styleguide-ui';
+import { InvoiceListComponent } from './pages/invoice-list/invoice-list.component';
+import { InvoiceListPageComponent } from './pages/invoice-list-page/invoice-list-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HealthCheckComponent,
+    InvoiceListComponent,
+    InvoiceListPageComponent,
   ],
   imports: [
     BrowserModule,
