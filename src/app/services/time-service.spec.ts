a955c28ee82322109ac869800d481c19e04a77ae
cah-joshua-dunn <joshua.dunn@cardinalhealth.com>
2021-05-14 14:02:18 -0400

    [FAL-261] Unit test refactor

--- src/app/services/time-service.spec.ts
+++ src/app/services/time-service.spec.ts
@@ -8,57 +8,51 @@ describe('UtilService Tests', () => {
 
   const testTimestamp = '2021-05-14T11:01:58.135Z';
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      providers: [
-        {
-          provide: TimeService, useFactory: () => {
-            return new TimeService('America/New_York');
+  describe('Timezone Provided', () => {
+    beforeEach(async () => {
+      await TestBed.configureTestingModule({
+        providers: [
+          {
+            provide: TimeService, useFactory: () => {
+              return new TimeService('America/New_York');
+            }
           }
-        }
-      ],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA]
-    }).compileComponents();
-    time = TestBed.inject(TimeService);
+        ],
+        schemas: [CUSTOM_ELEMENTS_SCHEMA]
+      }).compileComponents();
+      time = TestBed.inject(TimeService);
+    });
+
+    it('should create', () => {
+      expect(time).toBeTruthy();
+    });
+
+    it('should format a timestamp', () => {
+      const actual = time.formatTimestamp(testTimestamp, 'hh:mm');
+      expect(actual).toEqual('07:01');
+    });
+
+    it('should get the timezone', () => {
+      const actual = time.getTimezone;
+      expect(actual).toEqual('America/New_York');
+    });
   });
 
-  it('should create', () => {
-    expect(time).toBeTruthy();
-  });
+  describe('No Timezone Provided', () => {
+    beforeEach(async () => {
+      await TestBed.configureTestingModule({
+        providers: [TimeService],
+        schemas: [CUSTOM_ELEMENTS_SCHEMA]
+      }).compileComponents();
+      time = TestBed.inject(TimeService);
+    });
 
-  it('should format a timestamp', () => {
-    const actual = time.formatTimestamp(testTimestamp, 'hh:mm');
-    expect(actual).toEqual('07:01');
-  });
+    it('should format a timestamp using the set timezone', () => {
+      const actual = time.formatTimestamp(testTimestamp, 'hh:mm');
+      const expected = moment.tz(testTimestamp, time.getTimezone).format('hh:mm');
+      expect(actual).toEqual(expected);
+    });
 
-  it('should get the timezone', () => {
-    const actual = time.getTimezone;
-    expect(actual).toEqual('America/New_York');
-  });
-
-});
-
-describe('UtilService Tests with no specified timezone', () => {
-  let time: TimeService;
-
-  const testTimestamp = '2021-05-14T11:01:58.135Z';
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      providers: [TimeService],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA]
-    }).compileComponents();
-    time = TestBed.inject(TimeService);
-  });
-
-  it('should create', () => {
-    expect(time).toBeTruthy();
-  });
-
-  it('should format a timestamp using the set timezone', () => {
-    const actual = time.formatTimestamp(testTimestamp, 'hh:mm');
-    const expected = moment.tz(testTimestamp, time.getTimezone).format('hh:mm');
-    expect(actual).toEqual(expected);
   });
 
 });
