b6c2065925d4cfacab4ee3e4b1529b27bbd2f23b
cah-joshua-dunn <joshua.dunn@cardinalhealth.com>
2021-05-17 09:01:27 -0400

    [FAL-261] Adding inject annotation

--- src/app/services/time-service.ts
+++ src/app/services/time-service.ts
@@ -1,4 +1,4 @@
-import {Injectable, Optional} from '@angular/core';
+import {Inject, Injectable, Optional} from '@angular/core';
 import * as moment from 'moment-timezone';
 
 @Injectable()
@@ -6,7 +6,7 @@ export class TimeService {
 
   private readonly timezone: string = '';
 
-  constructor(@Optional() timezone?: string) {
+  constructor(@Optional() @Inject('') timezone?: string) {
     this.timezone = timezone || moment.tz.guess();
   }
 
