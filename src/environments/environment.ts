d6b5afdf411316a28dfc24a00b3aeb3d02936872
Sri Balusu <sri.balusu@LOH085YD0BQJG5J.cardinalhealth.net>
2021-06-07 13:06:56 -0400

    [FAL-300]-reverting changes for environment.ts file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: false,
-  // baseServiceUrl: 'https://elm-falcon-patient-gecko.apps.cf.np.gc.cahcloud.net'
-  baseServiceUrl: 'http://localhost:8080'
+  baseServiceUrl: 'https://elm-falcon-patient-gecko.apps.cf.np.gc.cahcloud.net'
 };
