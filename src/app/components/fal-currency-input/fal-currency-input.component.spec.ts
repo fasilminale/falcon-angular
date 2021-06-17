69c3c40ee3752a0b23b5705a21fce6d5e94ccbc9
Kooper Smith <kooper.smith@cardinalhealth.com>
2021-06-17 14:22:25 -0400

    [FAL-314] solution with tests

--- src/app/components/fal-currency-input/fal-currency-input.component.spec.ts
+++ src/app/components/fal-currency-input/fal-currency-input.component.spec.ts
@@ -1,22 +1,23 @@
-import {ComponentFixture, fakeAsync, TestBed, tick} from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { FalCurrencyInputComponent } from './fal-currency-input.component';
-import {FalRadioOrientation} from '../fal-radio-input/fal-radio-input.component';
-import {By} from '@angular/platform-browser';
-import {NgxCurrencyModule} from 'ngx-currency';
+import {FalCurrencyInputComponent, Selectable} from './fal-currency-input.component';
 
 describe('FalCurrencyInputComponent', () => {
   let component: FalCurrencyInputComponent;
   let fixture: ComponentFixture<FalCurrencyInputComponent>;
 
+  function mockSelectable(start: number | null, end: number | null): Selectable {
+    return {
+      selectionStart: start,
+      selectionEnd: end,
+      setSelectionRange: jasmine.createSpy()
+    };
+  }
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FalCurrencyInputComponent ]
-    })
-    .compileComponents();
-  });
-
-  beforeEach(() => {
+      declarations: [FalCurrencyInputComponent]
+    }).compileComponents();
     fixture = TestBed.createComponent(FalCurrencyInputComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -29,4 +30,56 @@ describe('FalCurrencyInputComponent', () => {
   it('should default to initialValue', () => {
     expect(component.value).toEqual('0.00');
   });
+
+  it('when a selection event occurs, should force text cursor to right of prefix', () => {
+    spyOn(component, 'forceCursorToRightOfPrefix').and.stub();
+    component.onSelectionEvent({target: {}});
+    expect(component.forceCursorToRightOfPrefix).toHaveBeenCalled();
+  });
+
+  describe('given a prefix of size 1', () => {
+    it('should have prefix of size 1', () => {
+      // this just makes sure we update the tests if the hard-coded prefix changes
+      // all of the other tests in this group assume this prefix length
+      expect(component.PREFIX.length).toEqual(1);
+    });
+
+    it(', should change 0,0 selection to 1,1', () => {
+      const input = mockSelectable(0, 0);
+      component.forceCursorToRightOfPrefix(input);
+      expect(input.setSelectionRange).toHaveBeenCalledWith(1, 1);
+    });
+
+    it(', should change 0,5 selection to 1,5', () => {
+      const input = mockSelectable(0, 5);
+      component.forceCursorToRightOfPrefix(input);
+      expect(input.setSelectionRange).toHaveBeenCalledWith(1, 5);
+    });
+
+    it(', should NOT change 1,1 selection', () => {
+      const input = mockSelectable(1, 1);
+      component.forceCursorToRightOfPrefix(input);
+      expect(input.setSelectionRange).not.toHaveBeenCalled();
+    });
+
+    it(', should NOT change 5,5 selection', () => {
+      const input = mockSelectable(5, 5);
+      component.forceCursorToRightOfPrefix(input);
+      expect(input.setSelectionRange).not.toHaveBeenCalled();
+    });
+
+    it(', should NOT change null,1 selection', () => {
+      const input = mockSelectable(null, 1);
+      component.forceCursorToRightOfPrefix(input);
+      expect(input.setSelectionRange).not.toHaveBeenCalled();
+    });
+
+    it(', should NOT change 1,null selection', () => {
+      const input = mockSelectable(1, null);
+      component.forceCursorToRightOfPrefix(input);
+      expect(input.setSelectionRange).not.toHaveBeenCalled();
+    });
+  });
+
+
 });
--- src/app/components/fal-currency-input/fal-currency-input.component.ts
+++ src/app/components/fal-currency-input/fal-currency-input.component.ts
@@ -8,13 +8,17 @@ import {FalControlValueAccessorComponent} from '../fal-control-value-accessor/fa
     <div class="form-group">
       <div class="input-group">
         <input id="currency-input"
-           type="text"
-           [ngClass]="isError ? 'error' : ''"
-           [disabled]="isDisabled"
-           class="form-control"
-           currencyMask
-           [options]="{ prefix: '$' }"
-           [(ngModel)]="value"
+               type="text"
+               [ngClass]="isError ? 'error' : ''"
+               [disabled]="isDisabled"
+               class="form-control"
+               currencyMask
+               [options]="{ prefix: PREFIX }"
+               [(ngModel)]="value"
+               (keydown)="onSelectionEvent($event)"
+               (keyup)="onSelectionEvent($event)"
+               (focus)="onSelectionEvent($event)"
+               (click)="onSelectionEvent($event)"
         />
       </div>
     </div>
@@ -30,6 +34,8 @@ import {FalControlValueAccessorComponent} from '../fal-control-value-accessor/fa
 })
 export class FalCurrencyInputComponent extends FalControlValueAccessorComponent<string> implements OnInit {
 
+  readonly PREFIX = '$';
+
   @Input() initialValue = '0.00';
   @Input() isDisabled = false;
   @Input() isError = false;
@@ -42,4 +48,38 @@ export class FalCurrencyInputComponent extends FalControlValueAccessorComponent<
     this.value = this.initialValue;
   }
 
+  /**
+   * An event handler for events that may effect text cursor selection.
+   */
+  onSelectionEvent(event: { target: any }): void {
+    this.forceCursorToRightOfPrefix(event.target as Selectable);
+  }
+
+  /**
+   * Forces the text cursor to the right of the prefix, if it isn't already.
+   * This is to alleviate issues with the currency mask, where it behaves strangely when the cursor is in that position.
+   */
+  forceCursorToRightOfPrefix(selectable: Selectable): void {
+    if (selectable.selectionStart != null
+      && selectable.selectionStart <= 0) {
+      const newStart = this.PREFIX.length;
+      const newEnd = selectable.selectionEnd != null
+      && selectable.selectionEnd >= this.PREFIX.length
+        ? selectable.selectionEnd
+        : this.PREFIX.length;
+      selectable.setSelectionRange(newStart, newEnd);
+    }
+  }
+
+}
+
+/**
+ * Represents an object that can have it's selection read and written.
+ * Mostly just needed for easy mocking during tests.
+ */
+export interface Selectable {
+  selectionStart: number | null;
+  selectionEnd: number | null;
+
+  setSelectionRange(newStart: number, newEnd: number): void;
 }
