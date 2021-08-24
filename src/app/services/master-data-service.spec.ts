4342fffb3e5985f6e76ee8acaa11c95577030ed1
cah-joshua-dunn <joshua.dunn@cardinalhealth.com>
2021-08-24 13:29:38 -0400

    [FAL-480] Adding Master Data View

---
+++ src/app/services/master-data-service.spec.ts
@@ -0,0 +1,45 @@
+import {fakeAsync, TestBed} from '@angular/core/testing';
+import {HttpTestingController} from '@angular/common/http/testing';
+import {WebServices} from './web-services';
+import {MasterDataService} from './master-data-service';
+import {environment} from '../../environments/environment';
+import {of} from 'rxjs';
+import {FalconTestingModule} from '../testing/falcon-testing.module';
+import {MasterDataRow} from '../models/master-data-row/master-data-row';
+
+describe('MasterDataService', () => {
+  let http: HttpTestingController;
+  let web: WebServices;
+  let masterDataService: MasterDataService;
+  let masterDataRows: Array<MasterDataRow>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [FalconTestingModule],
+    });
+    http = TestBed.inject(HttpTestingController);
+    web = TestBed.inject(WebServices);
+    masterDataService = TestBed.inject(MasterDataService);
+    masterDataRows = [
+      {
+        label: 'Business Units',
+        lastUpdated: '1970-01-01T10:00:00.000+0000',
+        endpoint: 'businessUnits',
+        hasDownloadableTemplate: true
+      }
+    ];
+  });
+
+  afterEach(() => {
+    http.verify();
+  });
+
+  describe('getBusinessUnit', () => {
+    it('should return businessUnit', fakeAsync(() => {
+      spyOn(web, 'httpGet').and.returnValue(of(masterDataRows));
+      const result = masterDataService.getMasterDataRows().toPromise();
+      expect(result).toBeTruthy();
+      expect(web.httpGet).toHaveBeenCalled();
+    }));
+  });
+});
--- src/app/services/time-service.ts
+++ src/app/services/time-service.ts
@@ -1,13 +1,35 @@
 import {Inject, Injectable, Optional} from '@angular/core';
 import * as moment from 'moment-timezone';
 
+export type formatType = 'imperial' | 'metric' | 'iso' | 'stopPage';
+
 @Injectable()
 export class TimeService {
+  private apiTimeFormat = 'YYYY-MM-DD HH:mm:ss Z';
+  private outputFormatToFormatStringMap = {
+    imperial: 'MM-DD-YYYY HH:mm z',
+    metric: 'DD-MM-YYYY HH:mm z',
+    iso: 'YYYY-MM-DD HH:mm z',
+    stopPage: 'ddd, MMM DD, YYYY'
+  };
 
   private readonly timezone: string = '';
 
   constructor(@Optional() @Inject('') timezone?: string) {
     this.timezone = timezone || moment.tz.guess();
+  }
+
+  convertFromApiTime(time?: string, timeZone?: string, outputFormat?: formatType): string {
+    if (!time) {
+      return '';
+    }
+    const outputFormatString = this.outputFormatToFormatStringMap[outputFormat ?? 'imperial']
+      ?? this.outputFormatToFormatStringMap.imperial;
+    if (!timeZone) {
+      timeZone = 'UTC';
+    }
+    const formattedTime = moment.utc(time, this.apiTimeFormat).tz(timeZone);
+    return formattedTime.format(outputFormatString);
   }
 
   get getTimezone(): string {
