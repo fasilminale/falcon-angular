1d8ee9d4a2653ccfba304229dffd749650ffe559
cah-joshua-dunn <joshua.dunn@cardinalhealth.com>
2021-08-24 15:30:53 -0400

    [FAL-480] Adding additional unit tests

--- src/app/services/time-service.spec.ts
+++ src/app/services/time-service.spec.ts
@@ -47,4 +47,72 @@ describe('TimeService', () => {
     });
   });
 
+  describe('convertFromApiTime', () => {
+    const year = 2020;
+    const month = '04';
+    const day = 15;
+    const hours = 20;
+    const minutes = 57;
+    const seconds = 56;
+    const baseUtcOffset = '+0000';
+    const timeString = `${year}-${month}-${day} ${hours}:${minutes}:${seconds} ${baseUtcOffset}`;
+
+    it('should convert provided timeString to provided timeZone: undefined', () => {
+      // Output should be in UTC
+      expect(time.convertFromApiTime(timeString, undefined))
+        .toEqual(`${month}-${day}-${year} ${hours}:${minutes} UTC`);
+      expect(time.convertFromApiTime(timeString, undefined, 'iso'))
+        .toEqual(`${year}-${month}-${day} ${hours}:${minutes} UTC`);
+      expect(time.convertFromApiTime(timeString, undefined, 'imperial'))
+        .toEqual(`${month}-${day}-${year} ${hours}:${minutes} UTC`);
+      expect(time.convertFromApiTime(timeString, undefined, 'metric'))
+        .toEqual(`${day}-${month}-${year} ${hours}:${minutes} UTC`);
+    });
+
+    it('should convert provided timeString to provided timeZone: US/Pacific', () => {
+      // PDT is GMT -7
+      const newHours = hours - 7;
+      expect(time.convertFromApiTime(timeString, 'US/Pacific'))
+        .toEqual(`${month}-${day}-${year} ${newHours}:${minutes} PDT`);
+      expect(time.convertFromApiTime(timeString, 'US/Pacific', 'iso'))
+        .toEqual(`${year}-${month}-${day} ${newHours}:${minutes} PDT`);
+      expect(time.convertFromApiTime(timeString, 'US/Pacific', 'imperial'))
+        .toEqual(`${month}-${day}-${year} ${newHours}:${minutes} PDT`);
+      expect(time.convertFromApiTime(timeString, 'US/Pacific', 'metric'))
+        .toEqual(`${day}-${month}-${year} ${newHours}:${minutes} PDT`);
+    });
+
+    it('should convert provided timeString to provided timeZone: America/New_York', () => {
+      // EST is GMT -4
+      const newHours = hours - 4;
+      expect(time.convertFromApiTime(timeString, 'America/New_York'))
+        .toEqual(`${month}-${day}-${year} ${newHours}:${minutes} EDT`);
+      expect(time.convertFromApiTime(timeString, 'America/New_York', 'iso'))
+        .toEqual(`${year}-${month}-${day} ${newHours}:${minutes} EDT`);
+      expect(time.convertFromApiTime(timeString, 'America/New_York', 'imperial'))
+        .toEqual(`${month}-${day}-${year} ${newHours}:${minutes} EDT`);
+      expect(time.convertFromApiTime(timeString, 'America/New_York', 'metric'))
+        .toEqual(`${day}-${month}-${year} ${newHours}:${minutes} EDT`);
+    });
+
+    it('should convert provided timeString to provided timeZone: Asia/Shanghai', () => {
+      // CST is GMT +8
+      const newHours = '0' + (hours - 16);
+      const newDay = day + 1;
+      expect(time.convertFromApiTime(timeString, 'Asia/Shanghai'))
+        .toEqual(`${month}-${newDay}-${year} ${newHours}:${minutes} CST`);
+      expect(time.convertFromApiTime(timeString, 'Asia/Shanghai', 'iso'))
+        .toEqual(`${year}-${month}-${day + 1} ${newHours}:${minutes} CST`);
+      expect(time.convertFromApiTime(timeString, 'Asia/Shanghai', 'imperial'))
+        .toEqual(`${month}-${day + 1}-${year} ${newHours}:${minutes} CST`);
+      expect(time.convertFromApiTime(timeString, 'Asia/Shanghai', 'metric'))
+        .toEqual(`${day + 1}-${month}-${year} ${newHours}:${minutes} CST`);
+    });
+
+    it('should not make any change when there is no provided time', () => {
+      expect(time.convertFromApiTime('')).toEqual('');
+      expect(time.convertFromApiTime(undefined)).toEqual('');
+    });
+  });
+
 });
