2ce46b1cc89ac23a195fa7a45f8a0bcf58546247
john.hamacher <john.hamacher@cardinalhealth.com>
2022-05-02 06:55:19 -0500

    [FAL-862] envirnment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,4 @@ export const environment = {
   production: false,
   jiraFeedbackCollectorId: 'fa1bf622',
   baseServiceUrl: 'https://elm-falcon-patient-gecko.apps.cf.np.gc.cahcloud.net'
-  // baseServiceUrl: 'http://localhost:8080'
 };
