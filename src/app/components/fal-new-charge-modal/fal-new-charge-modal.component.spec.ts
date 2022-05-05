4f382b0e5bb67101d5666ce853fddace8311569d
Kooper Smith <kooper.smith@cardinalhealth.com>
2022-05-05 15:39:35 -0400

    [FAL-744] new modal coverage

--- src/app/components/fal-new-charge-modal/fal-new-charge-modal.component.spec.ts
+++ src/app/components/fal-new-charge-modal/fal-new-charge-modal.component.spec.ts
@@ -3,8 +3,20 @@ import {FalNewChargeModalComponent} from './fal-new-charge-modal.component';
 import {FalconTestingModule} from '../../testing/falcon-testing.module';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
 import {SubjectValue} from '@elm/elm-styleguide-ui';
+import {CalcDetail} from '../../models/rate-engine/rate-engine-request';
 
 describe('FalCommentModalComponent', () => {
+  const TEST_VARIABLE_NAME = 'Test Variable';
+  const TEST_CALC_DETAIL: CalcDetail = {
+    accessorialCode: 'TEST',
+    name: 'Test Calc Detail',
+    variables: [
+      {
+        variable: TEST_VARIABLE_NAME,
+        quantity: 0
+      }
+    ]
+  };
 
   let component: FalNewChargeModalComponent;
   let fixture: ComponentFixture<FalNewChargeModalComponent>;
@@ -35,9 +47,43 @@ describe('FalCommentModalComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should confirm', () => {
+  it('should call on charge select when charge value changes', done => {
+    spyOn(component, 'onChargeSelect').and.stub();
+    component.chargeControl.valueChanges.subscribe(() => {
+      expect(component.onChargeSelect).toHaveBeenCalledOnceWith(TEST_CALC_DETAIL);
+      done();
+    });
+    component.chargeControl.setValue(TEST_CALC_DETAIL);
+  });
+
+  it('should give empty variable control names', () => {
+    const controlNames = component.getVariableControlNames();
+    expect(controlNames).toEqual([]);
+  });
+
+  it('should give variable control names', () => {
+    component.chargeControl.setValue(TEST_CALC_DETAIL);
+    const controlNames = component.getVariableControlNames();
+    expect(controlNames).toEqual([TEST_VARIABLE_NAME]);
+  });
+
+  it('should clear all variable controls', () => {
+    spyOn(component.variableControls, 'removeControl').and.callThrough();
+    component.chargeControl.setValue(TEST_CALC_DETAIL);
+    component.clearAllVariableControls();
+    expect(component.variableControls.removeControl).toHaveBeenCalledOnceWith(TEST_VARIABLE_NAME);
+  });
+
+  it('should close the dialog WITHOUT a response when a charge type is NOT selected', () => {
     spyOn(MOCK_DIALOG, 'close');
     component.confirm();
-    expect(MOCK_DIALOG.close).toHaveBeenCalled();
+    expect(MOCK_DIALOG.close).toHaveBeenCalledOnceWith(undefined);
+  });
+
+  it('should close the dialog with a response when a charge type is selected', () => {
+    component.chargeControl.setValue(TEST_CALC_DETAIL);
+    spyOn(MOCK_DIALOG, 'close');
+    component.confirm();
+    expect(MOCK_DIALOG.close).toHaveBeenCalledTimes(1);
   });
 });
