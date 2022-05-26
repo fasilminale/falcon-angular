70c57bb68d3f991175f84d9e2aff10bcf7bf4c23
cah-joshua-dunn <joshua.dunn@cardinalhealth.com>
2022-05-26 07:38:26 -0400

    [FAL-1007] Removing unused field

--- src/app/models/invoice/edit-auto-invoice.model.ts
+++ src/app/models/invoice/edit-auto-invoice.model.ts
@@ -1,13 +1,11 @@
 import {CarrierReference} from "../master-data-models/carrier-model";
 import {CarrierModeCodeReference} from "../master-data-models/carrier-mode-code-model";
 import {ServiceLevel} from "../master-data-models/service-level-model";
-import {CostLineItem} from '../line-item/line-item-model';
 
 export interface EditAutoInvoiceModel {
 
   carrier?: CarrierReference;
   mode?: CarrierModeCodeReference;
   serviceLevel?: ServiceLevel;
-  pickupDateTime?: string;
-  pendingChargeLineItems?: Array<CostLineItem>;
+  pickupDateTime?: string
 }
