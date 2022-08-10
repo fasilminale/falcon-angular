4d4e74417809e29a985aca717d4c6a7993843400
Sri Balusu <sri.balusu@MLC02YD0BQJG5J.cardinalhealth.net>
2022-08-10 09:06:03 -0400

    [FAL-874] error and disputed statuses; save and submit

--- src/app/models/invoice/invoice-model.ts
+++ src/app/models/invoice/invoice-model.ts
@@ -39,6 +39,7 @@ export class InvoiceDataModel {
   milestones: Array<Milestone>;
   deleted: boolean;
   failedToCreate: boolean;
+  payable: boolean;
 
   /* --- MANUAL INVOICE FIELDS --- */
   workType: string;
@@ -131,6 +132,7 @@ export class InvoiceDataModel {
     this.milestones = json?.milestones ?? [];
     this.deleted = !!json?.deleted;
     this.failedToCreate = !!json?.failedToCreate;
+    this.payable = !!json?.payable;
 
     // MANUAL INVOICE
     this.workType = json?.workType ?? '';
