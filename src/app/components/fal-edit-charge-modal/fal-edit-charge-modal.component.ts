80eb82cd533c8d18aa85dedf7d4059d2fafa5595
Kooper Smith <kooper.smith@cardinalhealth.com>
2022-10-04 11:26:59 -0400

    [FAL-1168] revert sonar fixes

--- src/app/components/fal-edit-charge-modal/fal-edit-charge-modal.component.ts
+++ src/app/components/fal-edit-charge-modal/fal-edit-charge-modal.component.ts
@@ -47,25 +47,19 @@ export class FalEditChargeModalComponent {
       this.commentControl.disable();
 
     }
-
-    this.fileName = this.parseAttachmentFileName(data);
-
+    const fullFileName = data?.costLineItem?.value?.attachment?.fileName;
+    if (data?.costLineItem?.value?.attachment?.fileName) {
+     const splitFileNameArray = fullFileName.split('~', 2);
+     if (splitFileNameArray.length === 2) {
+       this.fileName = data?.costLineItem?.value?.attachment.fileName.split('~', 2)[1];
+     } else {
+       this.fileName = fullFileName;
+     }
+    } else {
+      this.fileName = '';
+    }
     this.form.addControl('file', this.file);
     this.commentControl.enable();
-  }
-
-  parseAttachmentFileName(data: EditChargeModalInput): string{
-    let tempFileName = '';
-    const fullFileName = data?.costLineItem?.value?.attachment?.fileName;
-    if (fullFileName) {
-      const splitFileNameArray = fullFileName.split('~', 2);
-      if (splitFileNameArray.length === 2) {
-        tempFileName = data?.costLineItem?.value?.attachment.fileName.split('~', 2)[1];
-      } else {
-        tempFileName = fullFileName;
-      }
-    }
-    return tempFileName;
   }
 
   /**
