1337d0b9b80e2569e9ea56a18d4351e26e140dfc
cah-joshua-dunn <joshua.dunn@cardinalhealth.com>
2022-10-10 16:28:06 -0400

    [FAL-600] CSV Download WIP

--- src/app/components/fal-history-log-modal/fal-history-log-modal.component.spec.ts
+++ src/app/components/fal-history-log-modal/fal-history-log-modal.component.spec.ts
@@ -1,9 +1,13 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, fakeAsync, TestBed, tick} from '@angular/core/testing';
 
 import { FalHistoryLogModalComponent } from './fal-history-log-modal.component';
 import {FalconTestingModule} from '../../testing/falcon-testing.module';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
-import {SubjectValue} from '@elm/elm-styleguide-ui';
+import {ModalService, SubjectValue} from '@elm/elm-styleguide-ui';
+import {FormArray, FormControl} from '@angular/forms';
+import {of, throwError} from 'rxjs';
+import {WebServices} from '../../services/web-services';
+import * as saveAsFunctions from 'file-saver';
 
 describe('FalHistoryLogModalComponent', () => {
 
@@ -32,6 +36,8 @@ describe('FalHistoryLogModalComponent', () => {
   };
 
   let component: FalHistoryLogModalComponent;
+  let webService: WebServices;
+  let modalService: ModalService;
   let fixture: ComponentFixture<FalHistoryLogModalComponent>;
   let afterClosed$: SubjectValue<any>;
   let MOCK_DIALOG: any;
@@ -56,6 +62,8 @@ describe('FalHistoryLogModalComponent', () => {
   });
 
   beforeEach(() => {
+    webService = TestBed.inject(WebServices);
+    modalService = TestBed.inject(ModalService);
     fixture = TestBed.createComponent(FalHistoryLogModalComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -89,5 +97,29 @@ describe('FalHistoryLogModalComponent', () => {
     };
     component.toggleFullHistory([historyLog]);
     expect(component.filteredHistoryLogs.length).toEqual(1);
+  });
+
+  it('should download a list of history logs', () => {
+    spyOn(webService, 'httpPost').and.returnValue(of('csvData'));
+    spyOn(component, 'saveCSVFile').and.stub();
+    component.downloadCsv();
+    expect(component.saveCSVFile).toHaveBeenCalled();
+  });
+
+  it('should call saveAs', () => {
+    const saveAsSpy = spyOn(saveAsFunctions, 'saveAs').and.callFake(saveAs);
+    component.saveCSVFile('test data', 'test filename');
+    fixture.detectChanges();
+    expect(saveAsSpy).toHaveBeenCalled();
+  });
+
+  it('should display an error if downloading csv fails', () => {
+    spyOn(webService, 'httpPost').and.callFake(() => {
+      return throwError({error: JSON.stringify({error: { error: 'Test Exception', message: 'Test Exception'}})});
+    });
+    spyOn(modalService, 'openSystemErrorModal').and.returnValue(of());
+    component.downloadCsv();
+    fixture.detectChanges();
+    expect(modalService.openSystemErrorModal).toHaveBeenCalled();
   });
 });
