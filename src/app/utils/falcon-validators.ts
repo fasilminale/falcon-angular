c77d00d0c07f547450c98fafbb0333a935ffb981
Sri Balusu <sri.balusu@MLC02YD0BQJG5J.cardinalhealth.net>
2022-11-10 04:56:26 -0500

    [FAL-1409] bol number field validatin

--- src/app/utils/falcon-validators.ts
+++ src/app/utils/falcon-validators.ts
@@ -1,5 +1,12 @@
-import {Validators} from '@angular/forms';
+import {AbstractControl, ValidationErrors, Validators} from '@angular/forms';
 
 export const ALPHA_NUMERIC_REGEX = /^\w*$/;
 
 export const validateAlphanumeric = Validators.pattern(ALPHA_NUMERIC_REGEX);
+
+export class CustomValidators {
+    static requiredNonNA(control: AbstractControl): ValidationErrors | null {
+        let isValueEmpty = control.value === undefined || control.value === null || control.value === '' || control.value === 'N/A';
+        return isValueEmpty ? {'required': true} : null;
+    }
+}
