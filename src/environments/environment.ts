22164ab42c74b1bf25f3a1f18f30d2666f717d8d
ben schwemlein <ben.schwemlein@cardinalhealth.com>
2023-03-09 14:08:33 -0500

    [FAL-1721] Removed uneeded changes.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   jiraFeedbackCollectorId: 'fa1bf622',
-  baseServiceUrl: 'http://127.0.0.1:8080'
+  baseServiceUrl: 'https://elm-falcon-patient-gecko.apps.cf.np.gc.cahcloud.net'
 };
