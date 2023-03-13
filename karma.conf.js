8cd2f5bb2543e9e6eb9fa582f2addf307ed99259
Kooper Smith <kooper.smith@cardinalhealth.com>
2023-03-13 18:09:14 -0400

    [FAL-1837] prepare for upgrade to Angular 14

--- karma.conf.js
+++ karma.conf.js
@@ -33,14 +33,8 @@ module.exports = function(config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['ChromeHeadlessNoSandbox'],
-    customLaunchers: {
-      ChromeHeadlessNoSandbox: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox','--disable-setuid-sandbox']
-      }
-    },
+    autoWatch: false,
+    browsers: ['Chrome'],
     singleRun: true,
     captureTimeout: 21000000,
     browserDisconnectTolerance: 3,
